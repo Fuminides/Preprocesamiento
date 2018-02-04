72150d8fa15c755de851203f9348a2139b263e9a
Javier Fumanal <javierfumanalidocin@gmail.com>
2018-02-04 12:01:55 +0100

    Empesando

---
+++ utils.R
@@ -0,0 +1,20 @@
+
+load_train <- function(){
+	read.csv("./Datos/my_dataset_train.csv")
+}
+
+load_test <- function(){
+	read.csv("./Datos/my_dataset_test.csv")
+}
+
+write_prediction <- function(y_1){
+
+	y <- data.frame(seq(length(y_1)))
+	y$y <- y_1
+
+	names(y) <- c("Id", "Prediction")
+
+	write.table(y, file = "submit.csv",
+      quote = FALSE, row.names = FALSE,
+      append = FALSE, sep = ",")
+}
